78744170d520643db7c0cf3ef53efc88e3d347d4
inazrabuu <inazrabuu@gmail.com>
2024-05-10 17:20:55 +0700

    edit: add new controller sheet_filter

--- application/controllers/Sheet.php
+++ application/controllers/Sheet.php
@@ -27,7 +27,21 @@ class Sheet extends CI_Controller {
 		$google_sheet_helper = new Google_sheets_helper();
 
 		$url = 'https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vRDJuFnQs2MpjQeGoM6ccPu4TXEL1JuBmPVVJUrUWIBpyp-GVAZj_isSIOQZQEaXe1HtPdNuMqXtPde/pubhtml?gid=0&single=true';
-		$data['table'] = $google_sheet_helper->load_published_sheet($url);
+		$google_sheet_helper->load_published_sheet($url);
+		$data['table'] =  $google_sheet_helper->get_loaded_data();
+
+		$this->load->view('sheet', $data);
+	}
+
+	public function sheet_filter($param)
+	{
+		$this->load->helpers('google_sheets');
+
+		$google_sheet_helper = new Google_sheets_helper();
+
+		$url = 'https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vRDJuFnQs2MpjQeGoM6ccPu4TXEL1JuBmPVVJUrUWIBpyp-GVAZj_isSIOQZQEaXe1HtPdNuMqXtPde/pubhtml?gid=0&single=true';
+		$google_sheet_helper->load_published_sheet($url);
+		$data['table'] = $google_sheet_helper->filter(urldecode($param));
 
 		$this->load->view('sheet', $data);
 	}
