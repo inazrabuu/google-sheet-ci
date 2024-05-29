802c88dd3d5b3d3ccc3b719643066ae7051e49e2
inazrabuu <inazrabuu@gmail.com>
2024-05-29 15:35:57 +0700

    edit: move url, helper to constructor

--- application/controllers/Sheet.php
+++ application/controllers/Sheet.php
@@ -18,29 +18,33 @@ class Sheet extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+	protected $sheet, $sheet_delers_url, $sheet_stat_url;
+	public function __construct() {
+		parent::__construct();
+		$this->sheet_dealers_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYO2f8axAPLg2h9P2aBgG8gP-A6oJegH5W7b8E8ngWcy2S3puu2425SYqo3wxsMjao89EhQaZd0-Nb/pubhtml?gid=0&single=true';
+		$this->sheet_stat_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYO2f8axAPLg2h9P2aBgG8gP-A6oJegH5W7b8E8ngWcy2S3puu2425SYqo3wxsMjao89EhQaZd0-Nb/pubhtml?gid=99904499&single=true';
+
+		$this->load->helpers('google_sheets');
+		$this->sheet = new Google_sheets_helper();
+	}
+
 	public function index()
 	{
-		$this->load->helpers('google_sheets');
-
-		$google_sheet_helper = new Google_sheets_helper();
-
-		$url = 'https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vRDJuFnQs2MpjQeGoM6ccPu4TXEL1JuBmPVVJUrUWIBpyp-GVAZj_isSIOQZQEaXe1HtPdNuMqXtPde/pubhtml?gid=0&single=true';
-		$google_sheet_helper->load_published_sheet($url);
-		$data['table'] =  $google_sheet_helper->get_loaded_data();
+		$this->sheet->load_published_sheet($this->sheet_dealers_url);
+		$data['table'] =  $this->sheet->get_loaded_data();
 
 		$this->load->view('sheet', $data);
 	}
 
 	public function sheet_filter($param)
 	{
-		$this->load->helpers('google_sheets');
-
-		$google_sheet_helper = new Google_sheets_helper();
-
-		$url = 'https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vRDJuFnQs2MpjQeGoM6ccPu4TXEL1JuBmPVVJUrUWIBpyp-GVAZj_isSIOQZQEaXe1HtPdNuMqXtPde/pubhtml?gid=0&single=true';
-		$google_sheet_helper->load_published_sheet($url);
-		$data['table'] = $google_sheet_helper->filter(urldecode($param));
+		$this->sheet->load_published_sheet($this->sheet_dealers_url);
+		$data['table'] = $this->sheet->filter(urldecode($param));
 
 		$this->load->view('sheet', $data);
+	}
+
+	public function statistic() {
+
 	}
 }
